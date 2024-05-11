824880a0d85b985d6a9b02cd691b65099ac2b474
MMBKMC <151632121+MMBKMC@users.noreply.github.com>
2024-05-11 17:51:36 +0630

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import sys
+from sys import*
+
+def one_zero(sys):
+    '011',
+    '010',
+    '001',
+    '0101',
+    '110',
+    '011',
+    '01',
+    '0H',
+    '010',
+    '0E',
+    '001',
+    '0A',
+    '0I',
+    '111',
+    '0110',
+    '0P',
+    '0N',
+    '0S',
+    '0111',
+    '0I',
+    '0T',
+    '00E',
+    '0T0',
+    '0V0',
+    '0S0',
+    '0Y0'
+    
+A = "011 "
+B = "010 "
+C = "001 "
+D = "0101 "
+E = "110 "
+F = "011 "
+G = '01',
+H = '0H',
+I = '010',
+J = '0E',
+K = '001',
+L = '0A',
+M = '0I',
+N = '111',
+O ='0110',
+P = "0P",
+Q = "0N",
+R = "0S"
+S = "0111"
+T = "0I"
+U = "0T"
+V = "00E"
+W = "0T0"
+X = "0V0"
+Y = "0S0"
+Z = "0Y0"
+
+one = "A "
+two = "B "
+three = "C "
+four = "D "
+five = "E "
+six = "F "
+seven = "G "
+eight = "H "
+nine = "I "
+ten = "J "
+eleven = "K "
+twelve = "L "
+thirteen = "M "
+fourteen = "N "
+fifteen = "O "
+sixteen = "P "
+seventeen = "Q "
+eighteen = "R "
+nineteen = "S "
+Twenty = "T "
+Twenty_One = "U "
+Twenty_two = "V "
+Twenty_three = "W "
+Twenty_four = "X "
+Twenty_five = "Y "
+Twenty_six = "Z "
---
+++ runner.py
@@ -0,0 +1,7 @@
+import main
+from main import one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, Twenty, Twenty_One, Twenty_two, Twenty_three, Twenty_four, Twenty_five, Twenty_six
+
+print(eight + five + twelve + twelve + fifteen + Twenty_three + fifteen + eighteen + twelve + four )
+print(seven + fifteen + fifteen + four + Twenty_five + five + Twenty_three + fifteen + eighteen + twelve + four)
+
+open('test.tri','r').readlines()
